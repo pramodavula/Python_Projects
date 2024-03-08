4a3d78c445a81ba2b349505ed94d71f42b8b6d69
Pramod Avula <pramod.spce@gmail.com>
2024-03-07 23:15:08 -0600

    High Scores

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -14,7 +14,7 @@ class Settings:
 
         # Bullet settings
         #self.bullet_speed = 2.5
-        self.bullet_width = 3
+        self.bullet_width = 1000
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
@@ -24,6 +24,8 @@ class Settings:
 
         #How quickly the game speeds up
         self.speedup_scale = 1.1
+        #How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,10 +39,15 @@ class Settings:
         # Fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring settings
+        self.alien_points = 50
+
     def increase_speed(self):
-        """ Increase speed settings."""
+        """ Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
 
         
